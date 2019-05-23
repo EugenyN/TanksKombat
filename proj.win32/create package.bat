2414500f3ecb3dafe621cbfacfcb0a249396389f
Eugeny Novikov <nvikov@gmail.com>
2019-05-23 16:18:46 +0300

    Updated projects for support latest Visual Studio and Cocos2d-x SDK

--- proj.win32/create package.bat
+++ proj.win32/create package.bat
@@ -1,8 +1,4 @@
+if not exist ".\Packages\Tanks Kombat" mkdir ".\Packages\Tanks Kombat"
 xcopy ".\Release.win32\*.exe" ".\Packages\Tanks Kombat" /y /s
 xcopy ".\Release.win32\*.dll" ".\Packages\Tanks Kombat" /y /s
-xcopy ".\Release.win32\*.plist" ".\Packages\Tanks Kombat" /y /s
-xcopy ".\Release.win32\fonts" ".\Packages\Tanks Kombat\fonts" /y /s
-xcopy ".\Release.win32\graphics" ".\Packages\Tanks Kombat\graphics" /y /s
-xcopy ".\Release.win32\music\main.ogg" ".\Packages\Tanks Kombat\music\main.ogg" /y /s
-xcopy ".\Release.win32\sfx" ".\Packages\Tanks Kombat\sfx" /y /s
-pause
+xcopy ".\Release.win32\Resources" ".\Packages\Tanks Kombat\Resources" /y /s /i
